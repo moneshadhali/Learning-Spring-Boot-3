08bd4d4b483f663e590a70dc525153fe8830f906
Monesha <moneshadhali@gmail.com>
2024-11-18 23:23:58 +0000

    added new endpoints

--- 01-spring-boot-overview/mycoolapp/src/main/java/com/springboot/demo/mycoolapp/MycoolappApplication.java
+++ 01-spring-boot-overview/mycoolapp/src/main/java/com/springboot/demo/mycoolapp/MycoolappApplication.java
@@ -22,9 +22,20 @@ public class MycoolappApplication {
 	In summary, Maven simplifies the process of managing libraries and building your Java projects efficiently, acting as a “friendly helper” that streamlines development efforts.
 
 	mvnw and mvnw.cmd - checks to make sure the computer has maven installed. If they don't then install the maven (one file is for windows and the other for mac file)
-	src/main/jave - where the souce code resides
+	src/main/java - where the source code resides
 	src/main/resources - The src/main/resources directory in a Maven project is where you store configuration files and resources that your application needs at runtime. This could include properties files, XML files, and any other non-Java resources.
 	src/test - where the test files sources resides
 
+	For adding dependencies:
+	+ use spring-boot-starter-web
+	Dependencies for web + security:
+	+spring web
+	+spring security
+	+spring data jpa
+	+thymeleaf
+
+	To check whats inside the starter/dependencies select view>Tool Windows>Maven Projects>Dependencies
+
+	We can specify the java version we want to use specifically after spring boot starter parent by overriding inside the <properties> section eg: including <java.version>18</java.version>
 	 */
 }
--- 01-spring-boot-overview/mycoolapp/src/main/java/com/springboot/demo/mycoolapp/rest/FunRestController.java
+++ 01-spring-boot-overview/mycoolapp/src/main/java/com/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -10,4 +10,17 @@ public class FunRestController {
     public String getWelcome(){
         return "Welcome to Earth";
     }
+
+    //expose a new endpoint for "workout"
+
+    @GetMapping("/workout")
+    public String getWorkout(){
+        return "Run a hard 5K!";
+    }
+
+    // expose a new endpoint for "fortune"
+    @GetMapping("/fortune")
+    public String getFortune(){
+        return "Today is your lucky day.";
+    }
 }
