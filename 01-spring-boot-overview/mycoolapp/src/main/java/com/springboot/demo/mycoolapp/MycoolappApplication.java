39566c9abd5f1ddd8a6612278edac84345796fe1
Monesha <moneshadhali@gmail.com>
2024-11-19 14:51:24 +0000

    lesson headings included

--- 01-spring-boot-overview/mycoolapp/src/main/java/com/springboot/demo/mycoolapp/MycoolappApplication.java
+++ 01-spring-boot-overview/mycoolapp/src/main/java/com/springboot/demo/mycoolapp/MycoolappApplication.java
@@ -12,6 +12,7 @@ public class MycoolappApplication {
 
 	//Notes
 	/*
+	LESSON 1
 	Maven is a project management tool that helps developers manage their applications. Its primary uses include:
 
 	1. Dependency Management: Maven automatically handles downloading and including the necessary libraries (called JAR files) for your project. Instead of manually downloading each JAR from various websites, you just provide Maven with a list of what you need, and it fetches them for you.
@@ -21,11 +22,13 @@ public class MycoolappApplication {
 
 	In summary, Maven simplifies the process of managing libraries and building your Java projects efficiently, acting as a “friendly helper” that streamlines development efforts.
 
+	LESSON 2
 	mvnw and mvnw.cmd - checks to make sure the computer has maven installed. If they don't then install the maven (one file is for windows and the other for mac file)
 	src/main/java - where the source code resides
 	src/main/resources - The src/main/resources directory in a Maven project is where you store configuration files and resources that your application needs at runtime. This could include properties files, XML files, and any other non-Java resources.
 	src/test - where the test files sources resides
 
+	LESSON 3
 	For adding dependencies:
 	+ use spring-boot-starter-web
 	Dependencies for web + security:
@@ -34,8 +37,13 @@ public class MycoolappApplication {
 	+spring data jpa
 	+thymeleaf
 
+	LESSON 4
 	To check whats inside the starter/dependencies select view>Tool Windows>Maven Projects>Dependencies
 
+	LESSON 5
 	We can specify the java version we want to use specifically after spring boot starter parent by overriding inside the <properties> section eg: including <java.version>18</java.version>
+
+	LESSON 6
+	
 	 */
 }
