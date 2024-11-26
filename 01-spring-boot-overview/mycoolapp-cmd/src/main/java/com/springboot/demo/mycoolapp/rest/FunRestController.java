86349ed1edf30a2a402ebb1061cb7b39561b84ae
Monesha <moneshadhali@gmail.com>
2024-11-26 12:58:39 +0000

    java app added for cmd test purposes

---
+++ 01-spring-boot-overview/mycoolapp-cmd/src/main/java/com/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -0,0 +1,26 @@
+package com.springboot.demo.mycoolapp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+    @GetMapping("/")
+    public String getWelcome(){
+        return "Welcome to Earth";
+    }
+
+    //expose a new endpoint for "workout"
+
+    @GetMapping("/workout")
+    public String getWorkout(){
+        return "Run a hard 5K!";
+    }
+
+    // expose a new endpoint for "fortune"
+    @GetMapping("/fortune")
+    public String getFortune(){
+        return "Today is your lucky day.";
+    }
+}
