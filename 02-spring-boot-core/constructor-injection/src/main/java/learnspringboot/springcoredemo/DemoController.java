650dbd09b46be66012a49fe48baa0f2bde69e902
Monesha <moneshadhali@gmail.com>
2024-11-27 21:10:11 +0000

    class autowired for dependency injection

--- 02-spring-boot-core/constructor-injection/src/main/java/learnspringboot/springcoredemo/DemoController.java
+++ 02-spring-boot-core/constructor-injection/src/main/java/learnspringboot/springcoredemo/DemoController.java
@@ -6,5 +6,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoController {
 
+    //define a private field for the dependency
+    private Coach myCoach;
 
+    //define a constructor for dependency injection
+    @Autowired
+    public DemoController(Coach theCoach){
+        myCoach = theCoach;
+    }
 }
