a2989667c3c1ea4446239981469c7253f7d5d8f8
Monesha <moneshadhali@gmail.com>
2024-11-27 21:09:00 +0000

    DemoController created

---
+++ 02-spring-boot-core/constructor-injection/src/main/java/learnspringboot/springcoredemo/DemoController.java
@@ -0,0 +1,10 @@
+package learnspringboot.springcoredemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+
+}
